5bd44a1f8274a6a5cc960c8d0590cecdee32fb2a
neo <callmemaybeinstagram@gmail.com>
2026-03-07 08:15:08 +0000

    Modify Theme

--- lib/app.dart
+++ lib/app.dart
@@ -9,8 +9,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Colors.amber,
+
+        primaryColor: Colors.teal,
+        
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(1, 15, 140, 212)
+        ),
+
+        scaffoldBackgroundColor: Color.fromARGB(100, 54, 117, 180),
+
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(100, 170, 215, 5),
+          foregroundColor: Colors.black,
+        ),
+
       ),
+
+      darkTheme: ThemeData(
+        primaryColor: Colors.white,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(206, 169, 215, 5),
+          foregroundColor: Color.fromARGB(255, 255, 255, 255),
+        ),
+
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+          bodyLarge: TextStyle(color: Colors.white),
+          titleLarge: TextStyle(color: Colors.white),
+        ),
+
+      ),
+
+      themeMode: ThemeMode.system,
       home: MainScreen(),//
     );
   }
